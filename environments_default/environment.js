33942a60732470ca6a8f55f654729e690a88a018
TheDao032 <nthedao2705@gmail.com>
2021-07-22 21:12:12 +0700

    update environment

--- environments_default/environment.js
+++ environments_default/environment.js
@@ -6,11 +6,16 @@ const env = {
 		host: 'localhost',
 		port: 5432,
 		user: 'postgres',
-		password: '',
+		password: '2705',
 		dbName: 'FamilyStore_db',
 		minPool: 0,
 		maxPool: 50
-	}
+	},
+	secret: 'family_store_secret',
+	APP_ID: 'test_id',
+	APP_PASSWORD: 'test_password',
+	APP_SCOPE: '',
+	APP_REDIRECT_URI: 'localhost:3000'
 }
 
 module.exports = env
