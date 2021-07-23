6404086e5897ff53d9b4caed469205478aeda8bf
VoSiThien <vosithien1551999@gmail.com>
2021-07-23 22:21:40 +0700

    add, update

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -6,7 +6,7 @@ const successCode = 0
 const errorCode = 1
 
 router.get('/list', async (req, res) => {
-	const result = await knex.from('account')
+	const result = await knex.from('tbl_account')
 
 	if (result) {
 		return res.status(200).json({
@@ -37,9 +37,30 @@ router.get('/details/:id', async (req, res) => {
 	})
 })
 
-router.post('/add', (req, res) => {
-	const { firstName, lastName } = req.body
-	knex('account').insert({}).catch((error) => {
+router.post('/add', async (req, res) => {
+	const acc = req.body;
+	let date_ob = new Date();
+	if(!acc.username || !acc.password || !acc.email){
+		return res.status(400).json({
+			errorMessage: 'register faill',
+			code: errorCode
+		})
+	}
+	const result = await knex('tbl_account').max('acc_id as maxId').first()
+	const account = {
+		acc_id: +result['maxId'] + 1,
+		acc_username: acc.username,
+		acc_password: acc.password,
+		acc_email: acc.email,
+		acc_phone_number: acc.phone_number || null,
+		acc_full_name: acc.full_name || null,
+		acc_role: acc.role || 1,
+		acc_avatar: acc.avatar || null,
+		acc_status: acc.status || null,
+		acc_created_date: date_ob,
+		acc_updated_date: null
+	}
+	await knex('tbl_account').insert(account).catch((error) => {
 		return res.status(500).json({
 			errorMessage: error,
 			statusCode: errorCode
@@ -49,6 +70,39 @@ router.post('/add', (req, res) => {
 	return res.status(200).json({
 		statusCode: successCode
 	})
+})
+router.patch('/update', async (req, res) =>{
+	const acc = req.body;
+	let date_ob = new Date();
+	if(!acc.username || !acc.password || !acc.email){
+		return res.status(400).json({
+			errorMessage: 'register faill',
+			code: errorCode
+		});
+	}
+	const acc_id = acc.id;
+	const result = await knex('tbl_account').where('acc_id', acc_id);
+	const account = {
+		acc_username: acc.username || result['acc_username'],
+		acc_password: acc.password || result['acc_password'],
+		acc_email: acc.email || result['acc_email'],
+		acc_phone_number: acc.phone_number || result['acc_phone_number'],
+		acc_full_name: acc.full_name || result['acc_full_name'],
+		acc_role: acc.role || result['acc_role'],
+		acc_avatar: acc.avatar || result['acc_avatar'],
+		acc_status: acc.status || result['acc_status'],
+		acc_created_date: result['acc_created_date'],
+		acc_updated_date: date_ob
+	};
+	await knex('tbl_account').where('acc_id', acc_id).update(account).catch((error) => {
+		return res.status(500).json({
+			errorMessage: error,
+			statusCode: errorCode
+		})
+	});
+	return res.status(200).json({
+		statusCode: successCode
+	});
 })
 
 router.post('/delete/:id', (req, res) => {
