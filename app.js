8fd001ee585fb64a5722355868ddc90c966ee8b8
NguyenMinhTam-max <nguyenminhtam1106999@gmail.com>
2021-09-14 00:16:03 +0700

    create scheduled job to automatically update bill status, add filter to search

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const server = require('./server')
 const environment = require('./environments/environment')
+const scheduledJob = require('./schedule') // run schedule
+
+
+
 
 server.listen(environment.portServer, () => {
 	console.log(`Server Is Starting`)
